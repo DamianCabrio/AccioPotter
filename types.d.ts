52aa32e720e805845f036e8dcbeca6cb1f4f2f81
Damián <damiancabrio@hotmail.com>
2021-09-15 21:04:27 -0300

    Added character type

--- types.d.ts
+++ types.d.ts
@@ -4,3 +4,12 @@ type Book = {
   author: string;
   book_covers: Array;
 };
+
+type Character = {
+  id: number;
+  name: string;
+  birth: string;
+  death: string | null;
+  gender: string;
+  house: string;
+};
