0eefcb787dcd9519de6b3d0da95cccf6125586f1
Norbert <n0rb3r7@outlook.com>
2021-09-19 17:55:54 -0300

    Cleaning of styles on TitleBox component.

--- src/components/TitleBox/styles.ts
+++ src/components/TitleBox/styles.ts
@@ -13,30 +13,6 @@ const styles = StyleSheet.create({
     borderRadius: 48,
     paddingHorizontal: 15,
   },
-  primary: {
-    backgroundColor: colors.mainOrange,
-    borderColor: colors.mainOrange,
-  },
-  secondary: {
-    backgroundColor: colors.white,
-    borderColor: colors.mainOrange,
-  },
-  textInput: {
-    backgroundColor: colors.yellow,
-    color: colors.brown,
-    borderRadius: 48,
-    height: 43,
-    paddingHorizontal: 5,
-    width: '90%',
-  },
-  searchIcon: {
-    padding: 10,
-  },
 });
-
-export const buttonTextColors = {
-  primary: colors.white,
-  secondary: colors.mainOrange,
-};
 
 export default styles;
