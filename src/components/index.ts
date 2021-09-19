1052ee39f592e2a12a8f10b85420d39aeee48a53
Norbert <n0rb3r7@outlook.com>
2021-09-19 18:22:28 -0300

    Update components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 export { default as AlertModal } from './AlertModal';
 export { default as DefaultButton } from './DefaultButton';
+export { default as DescriptionBox } from './DescriptionBox';
 export { default as Header } from './Header';
 export { default as SearchBox } from './SearchBox';
 export { default as Separator } from './Separator';
 export { default as SimpleCard } from './SimpleCard';
-export { default as Typography } from './Typography';
 export { default as TitleBox } from './TitleBox';
+export { default as Typography } from './Typography';
